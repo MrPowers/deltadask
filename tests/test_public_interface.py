7737480f6711db7b2ff762104fc60203cd217e38
Matthew Powers <matthewkevinpowers@gmail.com>
2022-11-12 14:21:29 -0500

    Fix path

--- tests/test_public_interface.py
+++ tests/test_public_interface.py
@@ -4,5 +4,5 @@ import deltadask
 
 
 def test_read_reference_table1():
-    ddf = deltadask.read_delta("/Users/matthew.powers/Documents/code/my_apps/deltadask/tests/reference_tables/generated/reference_table_1/delta")
+    ddf = deltadask.read_delta("./tests/reference_tables/generated/reference_table_1/delta")
     print(ddf.compute())
