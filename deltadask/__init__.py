de933bfca1aadfbc784b9868cc15a53d84be3ee3
Matthew Powers <matthewkevinpowers@gmail.com>
2022-11-14 10:21:15 -0500

    Add files_by_partitions

--- deltadask/__init__.py
+++ deltadask/__init__.py
@@ -5,6 +5,9 @@ from deltalake import DeltaTable
 # filters: [[('col1', '==', 0), ...], ...]
 def read_delta(path, columns=None, filters=None):
     dt = DeltaTable(path)
-    filenames = [path + "/" + f for f in dt.files()]
+    if filters:
+        filenames = [path + "/" + f for f in dt.files_by_partitions(filters)]
+    else:
+        filenames = [path + "/" + f for f in dt.files()]
     ddf = dd.read_parquet(filenames, engine="pyarrow", columns=columns, filters=filters)
     return ddf
--- tests/test_public_interface.py
+++ tests/test_public_interface.py
@@ -6,3 +6,10 @@ import deltadask
 def test_read_reference_table1():
     ddf = deltadask.read_delta("./tests/reference_tables/generated/reference_table_1/delta")
     print(ddf.compute())
+
+
+def test_read_reference_table2():
+    ddf = deltadask.read_delta(
+        "./tests/reference_tables/generated/reference_table_2/delta", 
+        filters=[("letter", "=", "a")])
+    print(ddf.compute())
