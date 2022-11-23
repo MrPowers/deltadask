03b27030ffcec9069130a82f719aaa14d99a3030
Matthew Powers <matthewkevinpowers@gmail.com>
2022-11-23 17:14:31 -0500

    Update tests based on DAT framework

--- deltadask/__init__.py
+++ deltadask/__init__.py
@@ -3,11 +3,11 @@ from deltalake import DeltaTable
 
 # columns: ["col1", "col2"]
 # filters: [[('col1', '==', 0), ...], ...]
-def read_delta(path, columns=None, filters=None):
-    dt = DeltaTable(path)
+def read_delta(table_uri, version=None, storage_options=None, without_files=False, columns=None, filters=None):
+    dt = DeltaTable(table_uri, version, storage_options, without_files)
     if filters:
-        filenames = [path + "/" + f for f in dt.files_by_partitions(filters)]
+        filenames = [table_uri + "/" + f for f in dt.files_by_partitions(filters)]
     else:
-        filenames = [path + "/" + f for f in dt.files()]
+        filenames = [table_uri + "/" + f for f in dt.files()]
     ddf = dd.read_parquet(filenames, engine="pyarrow", columns=columns, filters=filters)
     return ddf
--- tests/test_public_interface.py
+++ tests/test_public_interface.py
@@ -7,16 +7,29 @@ import deltadask
 
 
 def test_read_reference_table1():
-    actual_ddf = deltadask.read_delta("./tests/reference_tables/generated/reference_table_1/delta")
-    df = pd.DataFrame({
-        "letter": ["a", "b", "c", "d", "e"],
-         "number": [1, 2, 3, 4, 5],
-          "a_float": [1.1, 2.2, 3.3, 4.4, 5.5]})
-    expected_ddf = dd.from_pandas(df, npartitions=2)
-    beavis.assert_dd_equality(actual_ddf, expected_ddf, check_index=False)
+    actual_ddf0 = deltadask.read_delta(
+        "./tests/reference_tables/generated/reference_table_1/delta",
+        version=0
+    )
+    expected_ddf0 = dd.read_parquet("tests/reference_tables/generated/reference_table_1/expected/v0/table_content.parquet")
+    beavis.assert_dd_equality(actual_ddf0, expected_ddf0, check_index=False, check_dtype=False)
+
+    actual_ddf1 = deltadask.read_delta(
+        "./tests/reference_tables/generated/reference_table_1/delta",
+        version=1
+    )
+    expected_ddf1 = dd.read_parquet("tests/reference_tables/generated/reference_table_1/expected/v1/table_content.parquet")
+    beavis.assert_dd_equality(actual_ddf1, expected_ddf1, check_index=False, check_dtype=False)
+
+    actual_ddf = deltadask.read_delta(
+        "./tests/reference_tables/generated/reference_table_1/delta"
+    )
+    expected_ddf = dd.read_parquet("tests/reference_tables/generated/reference_table_1/expected/latest/table_content.parquet")
+    beavis.assert_dd_equality(actual_ddf, expected_ddf, check_index=False, check_dtype=False)
 
 
 def test_read_reference_table2():
+    # TODO: Add this test to DAT
     actual_ddf = deltadask.read_delta(
         "./tests/reference_tables/generated/reference_table_2/delta", 
         filters=[("letter", "=", "a")])
@@ -25,4 +38,24 @@ def test_read_reference_table2():
         "a_float": [4.4, 1.1],
         "letter": ["a", "a"]})
     expected_ddf = dd.from_pandas(df, npartitions=2)
+    beavis.assert_dd_equality(actual_ddf, expected_ddf, check_index=False, check_dtype=False)
+
+    actual_ddf0 = deltadask.read_delta(
+        "./tests/reference_tables/generated/reference_table_2/delta",
+        version=0
+    )[["letter", "number", "a_float"]]
+    expected_ddf0 = dd.read_parquet("tests/reference_tables/generated/reference_table_2/expected/v0/table_content.parquet").sort_values("letter")
+    beavis.assert_dd_equality(actual_ddf0, expected_ddf0, check_index=False, check_dtype=False)
+
+    actual_ddf1 = deltadask.read_delta(
+        "./tests/reference_tables/generated/reference_table_2/delta",
+        version=1
+    )[["letter", "number", "a_float"]].sort_values("a_float")
+    expected_ddf1 = dd.read_parquet("tests/reference_tables/generated/reference_table_2/expected/v1/table_content.parquet").sort_values("a_float")
+    beavis.assert_dd_equality(actual_ddf1, expected_ddf1, check_index=False, check_dtype=False)
+
+    actual_ddf = deltadask.read_delta(
+        "./tests/reference_tables/generated/reference_table_2/delta"
+    )[["letter", "number", "a_float"]].sort_values("a_float")
+    expected_ddf = dd.read_parquet("tests/reference_tables/generated/reference_table_2/expected/latest/table_content.parquet").sort_values("a_float")
     beavis.assert_dd_equality(actual_ddf, expected_ddf, check_index=False, check_dtype=False)
