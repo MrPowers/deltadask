d8b4c90a2255f11ec1138bd6d63af6f55b84a5cd
Matthew Powers <matthewkevinpowers@gmail.com>
2023-05-09 07:16:46 -0400

    Run delta acceptance tests for all reference tables

--- deltadask/__init__.py
+++ deltadask/__init__.py
@@ -1,9 +1,17 @@
 import dask.dataframe as dd
 from deltalake import DeltaTable
 
+
 # columns: ["col1", "col2"]
 # filters: [[('col1', '==', 0), ...], ...]
-def read_delta(table_uri, version=None, storage_options=None, without_files=False, columns=None, filters=None):
+def read_delta(
+    table_uri,
+    version=None,
+    storage_options=None,
+    without_files=False,
+    columns=None,
+    filters=None,
+):
     dt = DeltaTable(table_uri, version, storage_options, without_files)
     if filters:
         filenames = [table_uri + "/" + f for f in dt.files_by_partitions(filters)]
--- tests/test_public_interface.py
+++ tests/test_public_interface.py
@@ -1,6 +1,7 @@
 import pytest
 import pandas as pd
 import dask.dataframe as dd
+import beavis
 
 import deltadask
 
@@ -10,38 +11,139 @@ def test_reader_all_primitive_types():
         "./tests/reader_tests/generated/all_primitive_types/delta"
     )
     expected_ddf = dd.read_parquet(
-        "tests/reader_tests/generated/all_primitive_types/expected/latest/table_content/*parquet")
+        "tests/reader_tests/generated/all_primitive_types/expected/latest/table_content/*parquet"
+    )
     pd.testing.assert_frame_equal(actual_ddf.compute(), expected_ddf.compute())
 
 
-# def test_read_reference_table2():
-#     # TODO: Add this test to DAT
+def test_reader_basic_append():
+    actual_ddf = deltadask.read_delta(
+        "./tests/reader_tests/generated/basic_append/delta"
+    )
+    expected_ddf = dd.read_parquet(
+        "tests/reader_tests/generated/basic_append/expected/latest/table_content/*parquet"
+    )
+    beavis.assert_pd_equality(
+        actual_ddf.compute().sort_values(by=["letter"]),
+        expected_ddf.compute(),
+        check_index=False,
+    )
+
+    actual_ddf = deltadask.read_delta(
+        "./tests/reader_tests/generated/basic_append/delta", version=0
+    )
+    expected_ddf = dd.read_parquet(
+        "tests/reader_tests/generated/basic_append/expected/v0/table_content/*parquet"
+    )
+    beavis.assert_pd_equality(
+        actual_ddf.compute().sort_values(by=["letter"]),
+        expected_ddf.compute(),
+        check_index=False,
+    )
+
+    actual_ddf = deltadask.read_delta(
+        "./tests/reader_tests/generated/basic_append/delta", version=1
+    )
+    expected_ddf = dd.read_parquet(
+        "tests/reader_tests/generated/basic_append/expected/v1/table_content/*parquet"
+    )
+    beavis.assert_pd_equality(
+        actual_ddf.compute().sort_values(by=["letter"]),
+        expected_ddf.compute(),
+        check_index=False,
+    )
+
+
+# def test_reader_basic_partitioned():
 #     actual_ddf = deltadask.read_delta(
-#         "./tests/reference_tables/generated/reference_table_2/delta", 
-#         filters=[("letter", "=", "a")])
-#     df = pd.DataFrame({
-#         "number": [4, 1],
-#         "a_float": [4.4, 1.1],
-#         "letter": ["a", "a"]})
-#     expected_ddf = dd.from_pandas(df, npartitions=2)
-#     beavis.assert_dd_equality(actual_ddf, expected_ddf, check_index=False, check_dtype=False)
-
-#     actual_ddf0 = deltadask.read_delta(
-#         "./tests/reference_tables/generated/reference_table_2/delta",
-#         version=0
-#     )[["letter", "number", "a_float"]]
-#     expected_ddf0 = dd.read_parquet("tests/reference_tables/generated/reference_table_2/expected/v0/table_content.parquet").sort_values("letter")
-#     beavis.assert_dd_equality(actual_ddf0, expected_ddf0, check_index=False, check_dtype=False)
-
-#     actual_ddf1 = deltadask.read_delta(
-#         "./tests/reference_tables/generated/reference_table_2/delta",
-#         version=1
-#     )[["letter", "number", "a_float"]].sort_values("a_float")
-#     expected_ddf1 = dd.read_parquet("tests/reference_tables/generated/reference_table_2/expected/v1/table_content.parquet").sort_values("a_float")
-#     beavis.assert_dd_equality(actual_ddf1, expected_ddf1, check_index=False, check_dtype=False)
-
+#         "./tests/reader_tests/generated/basic_partitioned/delta"
+#     )
+#     expected_ddf = dd.read_parquet(
+#         "tests/reader_tests/generated/basic_partitioned/expected/latest/table_content/*parquet"
+#     )
+#     beavis.assert_pd_equality(
+#         actual_ddf.compute().sort_values(by=["letter"]),
+#         expected_ddf.compute(),
+#         check_index=False,
+#     )
+#
 #     actual_ddf = deltadask.read_delta(
-#         "./tests/reference_tables/generated/reference_table_2/delta"
-#     )[["letter", "number", "a_float"]].sort_values("a_float")
-#     expected_ddf = dd.read_parquet("tests/reference_tables/generated/reference_table_2/expected/latest/table_content.parquet").sort_values("a_float")
-#     beavis.assert_dd_equality(actual_ddf, expected_ddf, check_index=False, check_dtype=False)
+#         "./tests/reader_tests/generated/basic_append/delta", version=0
+#     )
+#     expected_ddf = dd.read_parquet(
+#         "tests/reader_tests/generated/basic_append/expected/v0/table_content/*parquet"
+#     )
+#     print(actual_ddf.compute())
+#     beavis.assert_pd_equality(
+#         actual_ddf.compute().sort_values(by=["letter"]),
+#         expected_ddf.compute(),
+#         check_index=False,
+#     )
+
+def test_reader_nested_types():
+    actual_ddf = deltadask.read_delta(
+        "./tests/reader_tests/generated/nested_types/delta"
+    )
+    expected_ddf = dd.read_parquet(
+        "tests/reader_tests/generated/nested_types/expected/latest/table_content/*parquet"
+    )
+    pd.testing.assert_frame_equal(actual_ddf.compute(), expected_ddf.compute())
+
+
+def test_reader_no_replay():
+    actual_ddf = deltadask.read_delta(
+        "./tests/reader_tests/generated/no_replay/delta"
+    )
+    expected_ddf = dd.read_parquet(
+        "tests/reader_tests/generated/no_replay/expected/latest/table_content/*parquet"
+    )
+    pd.testing.assert_frame_equal(actual_ddf.compute(), expected_ddf.compute())
+
+
+def test_reader_no_stats():
+    actual_ddf = deltadask.read_delta(
+        "./tests/reader_tests/generated/no_stats/delta"
+    )
+    expected_ddf = dd.read_parquet(
+        "tests/reader_tests/generated/no_stats/expected/latest/table_content/*parquet"
+    )
+    pd.testing.assert_frame_equal(actual_ddf.compute(), expected_ddf.compute())
+
+
+def test_reader_stats_as_structs():
+    actual_ddf = deltadask.read_delta(
+        "./tests/reader_tests/generated/stats_as_struct/delta"
+    )
+    expected_ddf = dd.read_parquet(
+        "tests/reader_tests/generated/stats_as_struct/expected/latest/table_content/*parquet"
+    )
+    pd.testing.assert_frame_equal(actual_ddf.compute(), expected_ddf.compute())
+
+
+def test_reader_with_checkpoint():
+    actual_ddf = deltadask.read_delta(
+        "./tests/reader_tests/generated/with_checkpoint/delta"
+    )
+    expected_ddf = dd.read_parquet(
+        "tests/reader_tests/generated/with_checkpoint/expected/latest/table_content/*parquet"
+    )
+    pd.testing.assert_frame_equal(actual_ddf.compute(), expected_ddf.compute())
+
+
+def test_reader_with_schema_change():
+    actual_ddf = deltadask.read_delta(
+        "./tests/reader_tests/generated/with_schema_change/delta"
+    )
+    expected_ddf = dd.read_parquet(
+        "tests/reader_tests/generated/with_schema_change/expected/latest/table_content/*parquet"
+    )
+    pd.testing.assert_frame_equal(actual_ddf.compute(), expected_ddf.compute())
+
+
+    actual_ddf = deltadask.read_delta(
+        "./tests/reader_tests/generated/with_schema_change/delta", version=1
+    )
+    expected_ddf = dd.read_parquet(
+        "tests/reader_tests/generated/with_schema_change/expected/v1/table_content/*parquet"
+    )
+    pd.testing.assert_frame_equal(actual_ddf.compute(), expected_ddf.compute())
